173b7dabf55771212b806d70b99e7e0562631947
dgosling <daniellgosling@gmail.com>
2016-03-23 15:06:25 -0600

    Added health bar and damage taking

---
+++ Assets/scripts/enemydamage.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class enemydamage : MonoBehaviour {
+	public GameObject player;
+	public float damage;
+	public health value;
+	private float thealth1, thealth2,glob;
+	private GameObject refer;
+	//public double health;
+	// Use this for initiaelization
+	void Start () {
+		refer = GameObject.Find ("HealthBar");
+		value = refer.GetComponent<health> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<BoxCollider2D>().IsTouching(player.GetComponent<BoxCollider2D>())){
+			thealth1 = value.getchealth()-damage;
+			thealth2 = value.getmaxhealth();
+			value.setchealth (thealth1);
+			glob = thealth1/thealth2;
+			value.setBar (glob);
+		}
+	}
+
+
+}
---
+++ Assets/scripts/health.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class health : MonoBehaviour {
+
+	public float maxhealth = 100f;
+	public float chealth=0f;
+	public GameObject healthbar;
+	public float healthdec;
+
+
+	// Use this for initialization
+	void Start () {
+		chealth = maxhealth;
+		setBar (chealth/maxhealth);
+		//InvokeRepeating ("debug_dec", 1f,1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthdec = chealth / maxhealth;
+		setBar (healthdec);
+		
+	}
+
+	/*void debug_dec(){
+		chealth -= 2f;
+		healthdec = chealth / maxhealth;
+		setBar (healthdec);*/
+
+	public float getchealth(){
+		return chealth;
+	}
+
+	public float getmaxhealth(){
+		return maxhealth;
+	}
+	public void setchealth(float inp){
+		chealth = inp;
+	}
+
+
+	public void setBar(float myhealth){
+		
+		
+		healthbar.transform.localScale = new Vector3 (myhealth, healthbar.transform.localScale.y, healthbar.transform.localScale.z);
+	}
+}
