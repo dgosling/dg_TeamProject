712848153282678316f4dbcb5bc572eec9be03fb
dgosling <daniellgosling@gmail.com>
2016-04-05 17:09:16 -0600

    added groundpound

--- Assets/groundPound.cs
+++ Assets/groundPound.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 
 public class groundPound : MonoBehaviour {
-
+    public GameObject player;
+    public double height, velo, downwardsForce;
     
 
 
 	// Use this for initialization
 	void Start () {
-	
+        player = GameObject.FindGameObjectWithTag("Player");
+
 	}
 	
 	// Update is called once per frame
