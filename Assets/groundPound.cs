555eff3e8029437d1e30043ea18937fb3eb265de
dgosling <daniellgosling@gmail.com>
2016-04-05 17:59:04 -0600

    upd

--- Assets/groundPound.cs
+++ Assets/groundPound.cs
@@ -3,18 +3,47 @@ using System.Collections;
 
 public class groundPound : MonoBehaviour {
     public GameObject player;
-    public double height, velo, downwardsForce;
+    public float height, velo, downwardsForce,time,cvelo;
+    public playerInv inv;
     
 
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
-
+        inv = player.GetComponent<playerInv>();
+        
 	}
-	
+	void Update()
+    {
+        if(player.GetComponent<Rigidbody2D>().velocity.x>0)
+        {
+            cvelo = player.GetComponent<Rigidbody2D>().velocity.x;
+        }
+    }
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        StartCoroutine(gpound());
+        
 	}
+
+    IEnumerator gpound()
+    {
+        if (Input.GetButtonDown("Fire2") && inv.getElement(0) == true)
+        {
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            if (cvelo > 0)
+            {
+                player.GetComponent<Transform>().Translate(new Vector3(+velo, +0));
+            }
+                if (cvelo < 0)
+                player.GetComponent<Transform>().Translate(new Vector3(-velo, +0));
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(+velo, +height);
+
+            yield return new WaitForSeconds(time);
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(player.GetComponent<Rigidbody2D>().velocity.x, -(downwardsForce));
+        }
+    }
+    
+    
 }
