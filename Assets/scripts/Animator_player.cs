f41d96b6853a9979effd80f51d37da4628907f69
dgosling <daniellgosling@gmail.com>
2016-04-28 16:44:32 -0600

    Added animations

---
+++ Assets/scripts/Animator_player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animator_player : MonoBehaviour {
+    GameObject AoE;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        
+        anim = GetComponentInParent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject == GameObject.FindGameObjectWithTag("Player"))
+        {
+            anim.SetBool("Attack", true);
+
+        }
+
+        else
+            anim.SetBool("Attack", false);
+    }
+}
